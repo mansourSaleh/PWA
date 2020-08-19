050e47897713bed0dfe8afaa07869d3b1a86e76c
mansourSaleh <mansouralmohsen2018@gmai.com>
2020-08-19 21:17:50 +0300

    Updates

---
+++ precache-manifest.e58c6eed970ce79cd1cccffb4c05604a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144511b28adb77823aa5b0fabae8d959",
+    "url": "/PWA/index.html"
+  },
+  {
+    "revision": "49c59f49dff808b67ca2",
+    "url": "/PWA/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4d31f101302c2bfe551e",
+    "url": "/PWA/static/js/2.9ab29742.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PWA/static/js/2.9ab29742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49c59f49dff808b67ca2",
+    "url": "/PWA/static/js/main.5c1438e7.chunk.js"
+  },
+  {
+    "revision": "0db7a57c88fecaae21cf",
+    "url": "/PWA/static/js/runtime-main.1f81b076.js"
+  }
+]);
