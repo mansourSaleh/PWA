a449cd23bac65799099995674a6468af7ab2299c
mansourSaleh <mansouralmohsen2018@gmai.com>
2020-08-19 22:25:32 +0300

    added style

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function Contact() {
-    return (
-        <div className="app">
-            Contact
-        </div>
-    )
+  return (
+    <div className="app">
+
+        
+      <a href="https://api.whatsapp.com/send?phone=+966509111136" >
+        {" "}
+        <h1>whatsApp</h1>
+        +966-509111136
+      </a>
+
+      <h1>m0509111136@gmail.com</h1>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
